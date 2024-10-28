18eca124ba300b0bf8575add8077ac6d8e67d5f8
Alee <monarc1937@gmail.com>
2024-10-28 16:54:44 +0100

    Initier premier test au projet MMS

---
+++ cypress/e2e/mms-suite.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe("Formulaire inscription au club MMS", () => {
+  it("inscription nominale", () => {
+    cy.visit("index.html");
+  });
+});
+
+// Arrow functions <=> Fonctions fléchées () => { ...}
+// Fonction classique function() { .... }
